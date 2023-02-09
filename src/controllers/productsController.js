c612a9e4f653262f58ec4b2eca8d5efb3502eb53
Gus <gus_eze1988@hotmail.com>
2023-02-09 16:53:23 -0300

    Agrega crud eliminar, CRUD1 terminado

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -9,6 +9,9 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
+		const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		
         return res.render("products", {
             products,
 			toThousand
@@ -20,7 +23,7 @@ const controller = {
 		const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 		const {id} = req.params;
-		
+
 		const product = products.find(product => product.id === +id);
 		return res.render("./detail", {
 			...product,
@@ -98,6 +101,13 @@ const controller = {
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
 		// Do the magic
+		const {id} = req.params;
+
+		const productsModified = products.filter(product => product.id !== + id)
+		
+		fs.writeFileSync(productsFilePath, JSON.stringify(productsModified, null, 3), "utf-8");
+
+		return res.redirect("/products")
 	}
 };
 
